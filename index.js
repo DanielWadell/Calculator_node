f4a31e21b818e0beaa15b344616dd5a717384b9c
Daniel Wadell <danielwadell@Daniels-MacBook-Pro.local>
2021-10-11 16:50:42 -0700

    Testing continuous development on gcp

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello world!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+app.get('/add', function (req, res) {
+  res.send('Adding numbers together');
+});
+
+app.get('/subtract', function (req, res) {
+  res.send('Subtracting numbers from eachother');
+});
+
+app.get('/division', function (req, res) {
+  res.send('Dividing numbers by eachother');
+});
+
+app.get('/multiplication', function (req, res) {
+  res.send('Multiplying numbers together');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
